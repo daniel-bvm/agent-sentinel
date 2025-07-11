d49a6e2bfcc61f596ff0c1729b1d23b7d866ad50
George <george@bvm.network>
2025-07-11 17:58:51 +0700

    Update wait for Sonarqube

--- src/agent_sentinel/sonarqube_utils.py
+++ src/agent_sentinel/sonarqube_utils.py
@@ -1,7 +1,9 @@
 import subprocess
 import time
+import requests
 
 SONARQUBE_SHELL_SCRIPT = "/opt/sonarqube/bin/linux-x86-64/sonar.sh"
+SONARQUBE_URL = "http://localhost:9000"
 
 
 def start_sonarqube():
@@ -18,13 +20,26 @@ def stop_sonarqube():
     return result.stdout
 
 
+# TODO: Check the implementation of ElasticSearch without root user
+# {"id":"147B411E-AZf5H3Nfh00p6GNlGmc4","version":"10.4.1.88267","status":"UP"}
+# {"id":"147B411E-AZf5H3Nfh00p6GNlGmc4","version":"10.4.1.88267","status":"STARTING"}
 def _wait_for_sonarqube_to_start():
     # Wait for 10 seconds
     time.sleep(10)
     # Check if the sonarqube is running
-    command = f"sh {SONARQUBE_SHELL_SCRIPT} status"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return result.stdout
+
+    response = requests.get(f"{SONARQUBE_URL}/api/system/status")
+    if response.status_code == 200:
+        data = response.json()
+        if data["status"] == "UP":
+            return True
+        elif data["status"] == "STARTING":
+            return False
+        else:
+            return False
+    return False
+
+
 
 
 
