34caaa943c565e9ddfbfd7e8d7ae064995198ca9
George <george@bvm.network>
2025-07-22 16:37:37 +0700

    Update packaging script

--- pack.sh
+++ pack.sh
@@ -6,4 +6,4 @@ set -e
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
 rm -rf sentinel.zip
-zip -r sentinel.zip src config.json Dockerfile pyproject.toml system_prompt.txt app server.py install_trivy.sh
+zip -r sentinel.zip src config.json Dockerfile pyproject.toml system_prompt.txt app server.py install_trivy.sh greeting.txt
