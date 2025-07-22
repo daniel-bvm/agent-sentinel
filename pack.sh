6a9e54d022ec5f572eb84b5f33004963f6da74df
mac <mac@macs-Mac-Studio7.local>
2025-07-22 14:29:59 +0700

    update

--- pack.sh
+++ pack.sh
@@ -3,13 +3,7 @@
 # Exit immediately on error
 set -e
 
-# Check if Git working directory is clean
-if [[ -n $(git status --porcelain) ]]; then
-    echo "Error: Git working directory is not clean. Please commit or stash changes before proceeding."
-    exit 1
-fi
-
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
 rm -rf sentinel.zip
-zip -r sentinel.zip src config.json Dockerfile pyproject.toml system_prompt.txt app server.py
+zip -r sentinel.zip src config.json Dockerfile pyproject.toml system_prompt.txt app server.py install_trivy.sh
